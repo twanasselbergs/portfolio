1192d4c6be0919a131df0743c4c33ae44a9f4d72
TwanAsselbergs <twanasselbergs@outlook.com>
2024-12-13 17:24:54 +0100

    adjusted header

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grig md:grid md:grid-cols-[1fr,3fr,1fr]">
@@ -17,13 +19,19 @@ const Header = () => {
         </h1>
 
         <div className="relative md:justify-self-center">
-          <button className="menu-btn md:hidden" onClick={null}>
-            <span className="material-symbols-rounded">menu</span>
+          <button
+            className="menu-btn md:hidden"
+            onClick={() => setNavOpen((prev) => !prev)}>
+            <span className="material-symbols-rounded">
+              {navOpen ? "close" : "menu"}
+            </span>
           </button>
-          <Navbar />
+          <Navbar navOpen={navOpen} />
         </div>
 
-        <a href="#contact" className="">
+        <a
+          href="#contact"
+          className="btn btn-secondary max-md:hidden md:justify-self-end">
           Contact Me
         </a>
       </div>
