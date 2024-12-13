002c8079ccad0258e838f899819634696e0c1a19
TwanAsselbergs <twanasselbergs@outlook.com>
2024-12-13 01:57:19 +0100

    first commit

---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useRef } from "react";
+
+const Navbar = () => {
+  const lastActiveLink = useRef();
+
+  const navItems = [
+    {
+      label: "Home",
+      link: "#home",
+      className: "nav-link active",
+      ref: lastActiveLink,
+    },
+    {
+      label: "About",
+      link: "#about",
+      className: "nav-link",
+    },
+    {
+      label: "Work",
+      link: "#work",
+      className: "nav-link",
+    },
+    {
+      label: "Reviews",
+      link: "#reviews",
+      className: "nav-link",
+    },
+    {
+      label: "Contact",
+      link: "#contact",
+      className: "nav-link md:hidden",
+    },
+  ];
+
+  return (
+    <nav className="">
+      {navItems.map(({ label, link, className, ref }, key) => (
+        <a href={link} key={key} ref={ref} className={className} onClick={null}>
+          {label}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
